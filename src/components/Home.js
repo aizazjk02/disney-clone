707309ed22143f1df406b9add61b186025741f3c
aizazjk02 <56686849+aizazjk02@users.noreply.github.com>
2022-01-06 13:34:16 +0530

    Done With the viewers component

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,32 +1,32 @@
-import React from 'react'
-import BackgroundImg from "../images/home-background.png"
-import ImageSlider from './ImageSlider'
-import styled from 'styled-components'
+import React from "react";
+import BackgroundImg from "../images/home-background.png";
+import ImageSlider from "./ImageSlider";
+import styled from "styled-components";
+import { Viewers } from "./Viewers";
 const Home = () => {
-    return (
-        <Container>
-            <ImageSlider/>
-        </Container>
-    )
-}
+  return (
+    <Container>
+      <ImageSlider />
+      <Viewers />
+    </Container>
+  );
+};
 
 const Container = styled.main`
-    position: relative;
-    min-height: calc(100vh - 250px);
-    overflow-x: hidden;
-    display: block;
-    top: 72px;
-    padding: 0 calc(3.5vw + 5px);
+  position: relative;
+  min-height: calc(100vh - 250px);
+  overflow-x: hidden;
+  display: block;
+  top: 72px;
+  padding: 0 calc(3.5vw + 5px);
 
-    &:after {
-        background:url(${BackgroundImg}) center center / cover no-repeat fixed;
-        content: "";
-        position: absolute;
-        inset:0px;
-        opacity: 1;
-        z-index:-1;
-    }
-
-
-`
+  &:after {
+    background: url(${BackgroundImg}) center center / cover no-repeat fixed;
+    content: "";
+    position: absolute;
+    inset: 0px;
+    opacity: 1;
+    z-index: -1;
+  }
+`;
 export default Home;
